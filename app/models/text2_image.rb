e5300b17d4deb9ea1d3c7382c925409423e424d6
Marko Nikolic <nikolic89@hotmail.com>
2013-04-29 03:04:12 +0200

    blur effect on picture added

--- app/models/text2_image.rb
+++ app/models/text2_image.rb
@@ -4,15 +4,16 @@ class Text2Image < ActiveRecord::Base
 
 
   def create_image(text)
-	canvas = Magick::Image.new(300, 100){self.background_color = 'none'}
+	canvas = Magick::Image.new(576, 160){self.background_color = 'none'}
     gc = Magick::Draw.new
     gc.pointsize(25)
-    gc.text(30,70, text.center(14))
+    gc.text(10,20, text.center(14))
 
     gc.draw(canvas)
+    canvas = canvas.blur_channel(0, 2.7, Magick::AllChannels)
+
     filename = "#{Time.now.to_i.to_s}.png"
     path = "app/assets/images/test/#{filename}"
-   # canvas.blur=2 
     canvas.write(path)
 
     self.url = filename
